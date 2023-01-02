41b7c1a70ec616fde40556dc6706524889a915b7
Michael Holyland <7105903+mholyland@users.noreply.github.com>
2023-01-01 18:29:10 -0800

    first commit

---
+++ lib/red_lox.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "red_lox/version"
+
+module RedLox
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/red_lox_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RedLox do
+  it "has a version number" do
+    expect(RedLox::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
